103b13242a44650977eca9b60970e87c46007069
sushil bhatta <sushilbhatta58@gmail.com>
2025-02-21 23:55:46 +0545

    feat: add hero section and payment feature section

--- index.php
+++ index.php
@@ -5,68 +5,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logoipsum</title>
     <link rel="shortcut icon" href="./images/favicon.png">
-    <link rel="stylesheet" href="css/shared.css">
     <link rel="stylesheet" href="css/main.css">
     <script src="js/script.js" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+    <!-- backdrop -->
 <div class="backdrop"></div>
 <div class="background"></div>
+
+  <!-- desktop nav view -->
 <header class="main-header">
     <div class="nav-icon">
         <i class="fa-solid fa-2x fa-bars toggle-button"></i>
     </div>
+  
+    <div class="main-header__logo">
+        <a href="index.html" class="main-header__brand">
+            <img src="images/logo.png" alt="Logoipsum">
+        </a>
+    </div>
+      
+    <nav class="main-nav | fs-500 fw-semi-bold ">
+        <ul class="main-nav__items">
+            <li class="main-nav__item">
+                <a href="packages/index.html">Products</a>
+            </li>
+            <li class="main-nav__item">
+                <a href="customers/index.html">Solutions</a>
+            </li>
+            <li class="main-nav__item">
+                <a href="start-hosting/index.html">Help Center</a>
+            </li>
+            <li class="main-nav__item">
+                <a href="start-hosting/index.html">Get Started</a>
+            </li>
+        </ul>
+         
+    </nav>
+    <nav class="main-nav | fs-400">
+        <ul class="main-nav__items">
+            <li class="main-nav__item--cta__link">
+                <a href="login/index.html">Login</a>
+            </li>
+            <li class="main-nav__item--cta__primary">
+                <a  href="signup/index.html">Request a demo</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+    <!-- mobile nav -->
+<nav class="mobile-nav">
+    <div class="mobile-nav__header">
         <div class="main-header__logo">
-        
             <a href="index.html" class="main-header__brand">
                 <img src="images/logo.png" alt="Logoipsum">
             </a>
         </div>
-      
-        <nav class="main-nav | fs-500 fw-semi-bold ">
-            <ul class="main-nav__items">
-                <li class="main-nav__item">
-                    <a href="packages/index.html">Products</a>
-                </li>
-                <li class="main-nav__item">
-                    <a href="customers/index.html">Solutions</a>
-                </li>
-                <li class="main-nav__item">
-                    <a href="start-hosting/index.html">Help Center</a>
-                </li>
-                <li class="main-nav__item">
-                    <a href="start-hosting/index.html">Get Started</a>
-                </li>
-            </ul>
-         
-        </nav>
-        <nav class="main-nav | fs-400">
-        <ul class="main-nav__items">
-                <li class="main-nav__item--cta__link">
-                    <a href="login/index.html">Login</a>
-                </li>
-                <li class="main-nav__item--cta__primary">
-                    <a  href="signup/index.html">Request a demo</a>
-                </li>
-            </ul>
-        </nav>
-       
-    </header>
-    <nav class="mobile-nav">
-        <div class="mobile-nav__header">
-            <div class="main-header__logo">
                 
-                <a href="index.html" class="main-header__brand">
-                    <img src="images/logo.png" alt="Logoipsum">
-                </a>
-            </div>
-            <ul class="mobile-nav__items">
-                <li class="mobile-nav__item">
-                    <a href="packages/index.html">Products</a>
-                </li>
-                <li class="mobile-nav__item">
-            <a href="packages/index.html">Solutions</a>
+        <ul class="mobile-nav__items">
+            <li class="mobile-nav__item">
+                <a href="packages/index.html">Products</a>
+            </li>
+            <li class="mobile-nav__item">
+                <a href="packages/index.html">Solutions</a>
             </li>
             <li class="mobile-nav__item ">
                 <a href="packages/index.html">Help Center</a>
@@ -74,7 +76,6 @@
             <li class="mobile-nav__item">
                 <a href="packages/index.html">Get Started</a>
             </li>
-            
             <li class="mobile-nav__item mobile-nav__item--cta__link">
                 <a href="login/index.html">Login</a>
             </li>
@@ -82,11 +83,107 @@
                 <a  href="signup/index.html">Request a demo</a>
             </li>
         </ul>
-        
     </div>
 </nav>
-    <main>
+<main>
+<!-- hero section  view-->
+<section id="hero">
+    <div class="hero-content">
+        <h1 class="hero__heading">Get instant cash flow with invoice factoring</h1>
+        <p class="hero__text">Why wait? Get same day funding and a faster way to access cash flow.</p>
+            <a href="signup/index.html" class="btn btn--primary">Get Started</a>
+        <div class="hero-content__circles">
+            <span class="highlighted"></span>
+            <span></span>
+            <span ></span>
+        </div>
+    </div>
+    <div class="hero-img__container">
+        <img src="images/hero.png" alt="hero image" class="hero__image">
+    </div>
+    </section>
 
+    <!-- payment collection view -->
+    <section id="payment">
+        <header class="payment-heading"> 
+          <h2>Outsource payment collection</h2> 
+          <p>Faster and flexible access to cash flow from one or all your invoices.</p>
+        </header> 
+    <article class="payment-features"> 
+        <!-- one -->
+      <article class="payment-features__card">
+        <div class='payment-feature__card--layout'>
+                <header class="card-img"> 
+                    <img src="images/Earth.svg" alt="earth-logo"/> 
+                </header> 
+                <section class="card-info"> 
+                    <h3>Access up to $100,000</h3> 
+                    <p> We fund each invoice once approved and collect payment to optimise your cash flow.*</p> 
+                </section>   
+        </div>
+     </article> 
+     <!-- two -->
+     <article class="payment-features__card">
+        <div class='payment-feature__card--layout'>
+
+            <header class="card-img"> 
+                <img src="images/Earth.svg" alt="earth-logo"/> 
+            </header> 
+            <section class="card-info"> 
+                <h3>You choose invoices to get paid</h3> 
+                <p>Self-serve online portal available 24/7 or connect from your CRM or invoicing platform.​</p>         </section> 
+            </article> 
+            <!-- three -->
+        </div> 
+     <article class="payment-features__card">
+        <div class='payment-feature__card--layout'>
+
+            <header class="card-img"> 
+                <img src="images/Earth.svg" alt="earth-logo"/> 
+            </header> 
+            <section class="card-info"> 
+                <h3>Simple pricing</h3> 
+                <p>Only pay for what you use, if you don&apos;t need us, there are no costs.​</p>         </section> 
+            </article> 
+            <!-- four -->
+        </div> 
+     <article class="payment-features__card">
+        <div class='payment-feature__card--layout'>
+
+            <header class="card-img"> 
+                <img src="images/Earth.svg" alt="earth-logo"/> 
+            </header> 
+            <section class="card-info"> 
+                <h3>Click and quick</h3> 
+                <p>We fund each invoice once approved and collect payment to optimise your cash flow.*</p>         </section> 
+            </article> 
+            <!-- five -->
+        </div> 
+     <article class="payment-features__card">
+        <div class='payment-feature__card--layout'>
+
+            <header class="card-img"> 
+                <img src="images/Earth.svg" alt="earth-logo"/> 
+            </header> 
+            <section class="card-info"> 
+                <h3>Flexible</h3> 
+                <p>Self-serve online portal available 24/7 or connect from your CRM or invoicing platform.​</p>         </section> 
+            </article> 
+            <!-- six -->
+        </div> 
+     <article class="payment-features__card">
+     <div class='payment-feature__card--layout'>
+
+            <header class="card-img"> 
+                <img src="images/Earth.svg" alt="earth-logo"/> 
+            </header> 
+            <section class="card-info"> 
+                <h3>Invest in your business</h3> 
+                <p>Only pay for what you use, if you don’t need us, there are no costs.​</p>         </section> 
+            </article>
+        </article> 
+    </div>   
+ </section>
     </main>
 </body>
 </html>
